f6dacd8fe4d088626fd47faf9d37c31f4bfa6626
kirmiey017 <jamilhussaini017@gmail.com>
2026-07-05 16:16:58 +0100

    Create page.js

---
+++ app/request/[id]/page.js
@@ -0,0 +1,23 @@
+import { getRequest } from "../../../lib/db";
+import TicketView from "./TicketView";
+
+export default async function RequestPage({ params }) {
+  const item = getRequest(params.id);
+
+  if (!item) {
+    return (
+      <div className="wrap">
+        <div className="empty-state">
+          <div className="empty-title">Request not found</div>
+          <div>Double check the link, or submit a new request from the home page.</div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="wrap">
+      <TicketView item={item} />
+    </div>
+  );
+}
